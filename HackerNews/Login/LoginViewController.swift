9a6ea91c4a80b06d4f9cafcf894f2e63f5e3118e
Dhiraj Das <dhiraj.das@mutualmobile.com>
2018-03-31 11:40:28 +0530

    added login screen

---
+++ HackerNews/Login/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  HackerNews
+//
+//  Created by Dhiraj Das on 3/31/18.
+//  Copyright © 2018 Dhiraj Das. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var loginView: LoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
