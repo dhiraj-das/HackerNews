f0a865b8b9e8029d422d70f47147426a5e45a3d8
Dhiraj Das <dhiraj.das@mutualmobile.com>
2018-03-31 20:58:03 +0530

    customized the navigation bar. added uiview extensions and UINavigationBar extensions

--- HackerNews/Home/HomeViewController.swift
+++ HackerNews/Home/HomeViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet var homeView: HomeView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        customizeNavigationBar()
+    }
+    
+    private func customizeNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .orange
+        if let title = title {
+            navigationItem.leftBarButtonItem = UINavigationBar.buttonItem(withTitle: title, subtitle: "nil")
+        }
+    }
+    
+    private func updateLastUpdatedSubtitle() {
+        guard let navigationBarTitleView = navigationItem.leftBarButtonItem?.customView
+            as? NavigationBarTitleView else { return }
+        navigationBarTitleView.subTitle = "last updated"
+    }
+    
 }
